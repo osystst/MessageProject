e12747771ef113e6705879580c8e28aec519922c
osystst <osystst@gmail.com>
2020-04-27 20:35:08 +0900

    p.104 작업

--- src/main/java/app/messages/MessageService.java
+++ src/main/java/app/messages/MessageService.java
@@ -1,11 +1,23 @@
 package app.messages;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MessageService {
+
   private MessageRepository repository;
 
+  @Autowired
   public MessageService(MessageRepository repository) {
     this.repository = repository;
   }
+
+  // @Required
+  // public void setRepository(MessageRepository repository) {
+  //   this.repository = repository;
+  // }
 
   public void save(String text) {
     this.repository.saveMessage(new Message(text));
